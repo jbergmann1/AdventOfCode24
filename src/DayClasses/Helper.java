023e0295b2eb041ad7eaef663608eb56be5a2d7e
Jakob <jakobi.bergmann49@gmail.com>
2026-03-17 19:43:13 +0100

    helper printArrayNative changes

--- src/DayClasses/Helper.java
+++ src/DayClasses/Helper.java
@@ -40,7 +40,7 @@ public class Helper<T> {
     public void printArrayNative(T[][] array) {
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array[i].length; j++) {
-                System.out.print(array[j][i] + "  ");
+                System.out.print(array[i][j] + "  ");
             }
             System.out.println();
         }
